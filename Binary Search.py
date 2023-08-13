2bf31d26f4b8b1a43c02d4ad03b098ef8958e7b0
ksurbhi <94086790+ksurbhi@users.noreply.github.com>
2023-08-13 16:53:56 -0700

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -16,3 +16,40 @@ class Solution(object):
             else:
                 end = mid - 1
         return -1
+
+    
+    ##########################################################
+    ############# when array is in decending order ###########
+    def binary_search_decending_order_array(self, nums, ele):
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = int((start + end) /2)
+            if ele == nums[mid]:
+                return mid
+            elif  nums[mid] > ele:
+                start = mid+1
+            else:
+                end = mid - 1
+        return -1
+
+
+    ##########################################################
+    ############# when order of the array is not known #######
+    
+    def unorded_search(self, nums, ele):
+        start = 0
+        end = len(nums) - 1
+        if nums[start] < nums[start+1]:
+            self.search(nums, ele)
+        elif nums[start] > nums[start+1]:
+            self.binary_search_decending_order_array(self, nums, ele)
+            
+
+
+
+
+
+        
+        
+    
