ff5fea28cd2b6802ec1dc7dbfb1850c21069f3cf
ksurbhi <94086790+ksurbhi@users.noreply.github.com>
2024-01-29 14:25:25 -0800

    Valid Parenthesis

--- validParenthesis.py
+++ validParenthesis.py
@@ -1,3 +1,4 @@
+class Solution:
  def validParenthesis(self, str):
         stack = deque()
         for i in range(len(str)):
@@ -7,8 +8,11 @@
                 stack.appendleft('}')
             elif str[i] == '[':
                 stack.appendleft(']')
-            elif str[i] == stack[0]:
-                stack.popleft()
+            else:
+                if len(stack) ==0 or str[i] != stack[0] : # for cases like  '])' and '[)]'
+                    return False
+                elif str[i] == stack[0]: 
+                    stack.popleft()
         if len(stack) == 0:
             return True
         else:
@@ -19,14 +23,16 @@
         mapping = {"(": ")", "{": "}", "[": "]"}
         for char in str:
             if char in mapping:
-                stack.appendleft(mapping[char])
-            elif char == stack[0]:
-                stack.popleft()
+                stack.appendleft(mapping[char])           
+            else:
+                if len(stack) ==0 or char != stack[0]: # for cases like  '])' and '[)]'
+                    return False
+                elif char == stack[0]:
+                    stack.popleft()               
         if len(stack) == 0:
             return True
         else:
             return False
-
 
 arr = '[{()}]('
 cls_obj = Solution()
